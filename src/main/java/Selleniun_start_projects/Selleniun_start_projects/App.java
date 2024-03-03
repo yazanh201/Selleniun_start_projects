9a481e99801643266ac511d231b2be2b199531e7
chaya7282 <52631833+chaya7282@users.noreply.github.com>
2024-03-03 08:40:07 +0200

    Initial commit

---
+++ src/main/java/Selleniun_start_projects/Selleniun_start_projects/App.java
@@ -0,0 +1,13 @@
+package Selleniun_start_projects.Selleniun_start_projects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
